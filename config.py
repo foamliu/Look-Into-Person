fdb92c248f03235aa8ecf75b1c2268706fc103ce
Yang Liu <foamliu@yeah.net>
2018-05-28 16:31:27 +0800

    fix missing file error

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ num_train_samples = 28280
 num_valid_samples = 5000
 num_classes = 20
 
-mat = scipy.io.loadmat('data/instance-level_human_parsing/human_colormap.mat')
+mat = scipy.io.loadmat('human_colormap.mat')
 color_map = (mat['colormap'] * 256).astype(np.int32)
