845c44e64859a2649d1dec61e0e46134022431c9
Yang Liu <foamliu@yeah.net>
2018-07-03 22:42:39 +0800

    updaate

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ patience = 50
 num_train_samples = 28280
 num_valid_samples = 5000
 num_classes = 20
+weight_decay = 1e-2
 
 mat = scipy.io.loadmat('human_colormap.mat')
 color_map = (mat['colormap'] * 256).astype(np.int32)
