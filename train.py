f8fb6b6588fc2e6dc88c45fd6e7ff4d167ff3682
unknown <foamliu@yeah.net>
2018-11-02 11:10:24 +0800

    update

--- train.py
+++ train.py
@@ -52,10 +52,9 @@ if __name__ == '__main__':
         if pretrained_path is not None:
             new_model.load_weights(pretrained_path)
 
-    sgd = keras.optimizers.SGD(lr=1e-5, decay=1e-6, momentum=0.9, nesterov=True)
-    # new_model.compile(optimizer='adam', loss=cross_entropy)
-    # new_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    new_model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    adam = keras.optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.99, epsilon=1e-08, decay=5E-6)
+    # sgd = keras.optimizers.SGD(lr=1e-5, decay=1e-6, momentum=0.9, nesterov=True)
+    new_model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
     print(new_model.summary())
 
