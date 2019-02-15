d7a9351eeff0ce02203636b0f3aa8914913d542b
Yang Liu <liuyang12@focusmedia.cn>
2019-02-16 00:36:48 +0800

    update

--- utils.py
+++ utils.py
@@ -31,8 +31,11 @@ def cross_entropy(y_true, y_pred):
     return cross_ent
 
 
-def focal_loss(gamma=2., alpha=.25):
-    def focal_loss_fixed(y_true, y_pred):
+def focal_loss(gamma=2, alpha=0.75):
+    def focal_loss_fixed(y_true, y_pred):  # with tensorflow
+        eps = 1e-12
+        # improve the stability of the focal loss and see issues 1 for more information
+        y_pred = K.clip(y_pred, eps, 1. - eps)
         pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
         pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
         return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum(
