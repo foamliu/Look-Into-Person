dfbf1a903e6be68953873e722e27e0ef2e746a40
Sloan <sloan.lipman@gmail.com>
2020-09-10 17:34:26 -0400

    Move home to components directory

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule)
+    loadChildren: () => import('./components/home/home.module').then((m) => m.HomePageModule)
   },
   {
     path: '',
