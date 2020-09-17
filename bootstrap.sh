aaaae1a8561b17436a1a80c836be839a8c1307be
Sloan <sloan.lipman@gmail.com>
2020-09-17 17:28:24 -0400

    Modify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 export FLASK_APP=$PWD/app/__init__.py
 export FLASK_ENV=development
-source $(pipenv --venv)/bin/activate
+source myenv/bin/activate # Change this to the location of your own virtual environment!
+pip install -r requirements.txt
 flask run -h 0.0.0.0
