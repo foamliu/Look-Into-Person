23c7897ee8c36995db5a940cce276fee4b2e27ad
Sloan <sloan.lipman@gmail.com>
2020-09-23 20:42:19 -0400

    Add support for ngx-color-picker
    Add ngx-color-picker as a dependency. Import its ColorPickerModule in
    the HomeModule.

--- web/src/app/components/home/home.module.ts
+++ web/src/app/components/home/home.module.ts
@@ -3,12 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ColorPickerModule } from 'ngx-color-picker';
 import { ImageService } from 'src/app/services/image/image.service';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, HttpClientModule],
+  imports: [ColorPickerModule, CommonModule, FormsModule, IonicModule, HomePageRoutingModule, HttpClientModule],
   declarations: [HomePage],
   providers: [ImageService]
 })
