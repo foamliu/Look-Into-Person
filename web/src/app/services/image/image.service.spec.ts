b49d2cef0098267fa12e4779ec8a92f096ba4400
Sloan <sloan.lipman@gmail.com>
2020-09-24 20:11:42 -0400

    Clean up ImageService test suite
    Stop testing the uploadImage() method in the fakeAsync zone. In commit
    bbd7225, we changed the responseType of the /upload API from blob to
    JSON. We previously tested this method as fakeAsync because it was
    required for this response type. As the response type has changed, the
    test can be simplified to remove this code.

--- web/src/app/services/image/image.service.spec.ts
+++ web/src/app/services/image/image.service.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ImageService } from './image.service';
 
 describe('ImageService', () => {
@@ -24,23 +24,22 @@ describe('ImageService', () => {
 
   afterEach(() => httpTestingController.verify());
 
-  test('Should accept an image to upload and return a response', fakeAsync(() => {
-    const body = { segmentedImage: 'mockSrc' };
+  test('Should accept an image to upload and return a response', () => {
+    const expectedResponse = { segmentedImage: 'mockSrc' };
     const src = 'mockSrc';
 
     const expectedForm = new FormData();
     expectedForm.append('image', src);
     expectedForm.append('fileName', file.name);
 
-    service.uploadImage(src, file.name).subscribe((response) => expect(response).toEqual(body));
-    tick();
+    service.uploadImage(src, file.name).subscribe((response) => expect(response).toEqual(expectedResponse));
 
     const req = httpTestingController.expectOne('/upload');
     expect(req.request.method).toBe('POST');
     expect(req.request.body).toEqual(expectedForm);
 
-    req.flush(body);
-  }));
+    req.flush(expectedResponse);
+  });
 
   test('Should request outlined images with segment color, outline color, and outline thickness', () => {
     const expectedResponse = {
