e8ff3e487dd2f0821424b16f1ec908ba55c9bc75
Sloan <sloan.lipman@gmail.com>
2020-10-05 17:25:55 -0400

    Remove unneeded files
    Remove files related to training the model or providing a demo. These
    are unneeded for the automatic-apparel-outliner application.

--- config.py
+++ config.py
@@ -3,17 +3,7 @@ import numpy as np
 
 img_rows, img_cols = 320, 320
 channel = 3
-batch_size = 4
-epochs = 50
-patience = 50
-# num_train_samples = 1722
-# num_valid_samples = 574
-# num_train_samples = 30462
-# num_valid_samples = 10000
-num_train_samples = 53
-num_valid_samples = 26
 num_classes = 20
-# weight_decay = 1e-3
 
 mat = scipy.io.loadmat('human_colormap.mat')
 color_map = (mat['colormap'] * 256).astype(np.int32)
--- data_generator.py
+++ data_generator.py
@@ -5,17 +5,7 @@ import cv2 as cv
 import numpy as np
 from keras.utils import Sequence
 from keras.utils import to_categorical
-from config import batch_size, img_rows, img_cols, num_classes, color_map
-
-# train_images_folder = 'data/instance-level_human_parsing/Training/LIP_Images'
-# train_categories_folder = 'data/instance-level_human_parsing/Training/LIP_Category_ids'
-# valid_images_folder = 'data/instance-level_human_parsing/Validation/LIP_Images'
-# valid_categories_folder = 'data/instance-level_human_parsing/Validation/LIP_Category_ids'
-
-train_images_folder = 'data/instance-level_human_parsing/Training/Images'
-train_categories_folder = 'data/instance-level_human_parsing/Training/Category_ids'
-valid_images_folder = 'data/instance-level_human_parsing/Validation/Images'
-valid_categories_folder = 'data/instance-level_human_parsing/Validation/Category_ids'
+from config import img_rows, img_cols,  color_map
 
 
 def get_category(categories_folder, name):
@@ -29,7 +19,6 @@ def to_bgr(y_pred):
     for r in range(320):
         for c in range(320):
             color_id = y_pred[r, c]
-            # print("color_id: " + str(color_id))
             ret[r, c, :] = color_map[color_id]
     ret = ret.astype(np.uint8)
     return ret
@@ -53,72 +42,3 @@ def safe_crop(mat, x, y):
     h, w = crop.shape[:2]
     ret[0:h, 0:w] = crop
     return ret
-
-
-class DataGenSequence(Sequence):
-    def __init__(self, usage):
-        self.usage = usage
-
-        if usage == 'train':
-          # id_file = 'data/instance-level_human_parsing/Training/LIP_train_small_dataset.txt'
-          # id_file = 'data/instance-level_human_parsing/Training/LIP_train_id.txt'
-          id_file = 'data/instance-level_human_parsing/Training/train_id.txt'
-          self.images_folder = train_images_folder
-          self.categories_folder = train_categories_folder
-        else:
-            # id_file = 'data/instance-level_human_parsing/Validation/LIP_val_small_dataset.txt'
-            # id_file = 'data/instance-level_human_parsing/Validation/LIP_val_id.txt'
-            id_file = 'data/instance-level_human_parsing/Validation/val_id.txt'
-            self.images_folder = valid_images_folder
-            self.categories_folder = valid_categories_folder
-
-        with open(id_file, 'r') as f:
-            self.names = f.read().splitlines()
-
-        np.random.shuffle(self.names)
-
-    def __len__(self):
-        return int(np.ceil(len(self.names) / float(batch_size)))
-
-    def __getitem__(self, idx):
-        i = idx * batch_size
-
-        length = min(batch_size, (len(self.names) - i))
-        batch_x = np.empty((length, img_rows, img_cols, 3), dtype=np.float32)
-        batch_y = np.empty((length, img_rows, img_cols, num_classes), dtype=np.float32)
-
-        for i_batch in range(length):
-            name = self.names[i]
-            filename = os.path.join(self.images_folder, name + '.jpg')
-            image = cv.imread(filename)
-            image_size = image.shape[:2]
-            category = get_category(self.categories_folder, name)
-
-            x, y = random_choice(image_size)
-            image = safe_crop(image, x, y)
-            category = safe_crop(category, x, y)
-
-            if np.random.random_sample() > 0.5:
-                image = np.fliplr(image)
-                category = np.fliplr(category)
-
-            x = image / 255.
-            y = category
-
-            batch_x[i_batch, :, :, 0:3] = x
-            batch_y[i_batch, :, :] = to_categorical(y, num_classes)
-
-            i += 1
-
-        return batch_x, batch_y
-
-    def on_epoch_end(self):
-        np.random.shuffle(self.names)
-
-
-def train_gen():
-    return DataGenSequence('train')
-
-
-def valid_gen():
-    return DataGenSequence('valid')
