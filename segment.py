9f96d0e2ed4d9231e6e631cf68fc14dc1d936972
Sloan <sloan.lipman@gmail.com>
2020-10-14 19:25:29 -0400

    Modify segment.py

--- segment.py
+++ segment.py
@@ -1,38 +1,24 @@
 import os
-
-import keras.backend as K
-import numpy as np
 from PIL import Image
+from app.process.segnet.config import num_classes, img_rows, img_cols
+from app.process.segnet.data_generator import random_choice, safe_crop, to_bgr
+from app.process.segnet.model import build_model
+import numpy as np
 
-from app.segment.data_generator import random_choice, safe_crop, to_bgr
-from app.segment.model import build_model
-from app.segment.config import num_classes, img_rows, img_cols
-
-def segment_image(filename):
-
-    model_weights_path = './app/segment/models/model.hdf5'
+def img_process(path):
     model = build_model()
-    model.load_weights(model_weights_path)
+    weights = os.path.join(os.getcwd(), 'app', 'process', 'segnet', 'models', 'model.11-0.8409.hdf5')
+    model.load_weights(weights)
 
-    filename = 'app/img/uploads/' + filename
-    image = Image.open(filename)
-
-    size = (img_rows, img_cols)
-    image = image.resize(size)
-
+    image = np.array(Image.open(path))
     image_size = image.shape[:2]
-
     x, y = random_choice(image_size)
     image = safe_crop(image, x, y)
-
     x_test = np.empty((1, img_rows, img_cols, 3), dtype=np.float32)
-    x_test[0, :, :, 0:3] = image / 255.
-
+    x_test[0, :, :, 0:3] = image / 255
     out = model.predict(x_test)
     out = np.reshape(out, (img_rows, img_cols, num_classes))
     out = np.argmax(out, axis=2)
     out = to_bgr(out)
-
-    K.clear_session()
 
     return out
